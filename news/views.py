e5ed7caec34c09cd4a8a5889361409dca00d17e2
JaguarKovalev <jaguarkovalev@gmail.com>
2024-12-07 15:10:28 +0300

    Add news list view with pagination

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+def news_list(request):
+    news_items = News.objects.all()
+    paginator = Paginator(news_items, 4)  # 4 новости на страницу
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'news/news_list.html', {'page_obj': page_obj})
