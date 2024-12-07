bac7509ff61ea0fc2eb9cdf2b59395641800eccf
JaguarKovalev <jaguarkovalev@gmail.com>
2024-12-07 14:25:16 +0300

    Add model for storing quadratic equation solutions

--- solver/models.py
+++ solver/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class QuadraticSolution(models.Model):
+    a = models.FloatField()
+    b = models.FloatField()
+    c = models.FloatField()
+    user_solution = models.CharField(max_length=255)
+    correct_solution = models.CharField(max_length=255)
+    is_correct = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Solution for {self.a}x² + {self.b}x + {self.c} = 0"
