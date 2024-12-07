6aefca2a109e0fbb0d433b0dff2005ff85706e3d
JaguarKovalev <jaguarkovalev@gmail.com>
2024-12-07 15:09:38 +0300

    Customize News admin interface

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_content', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
