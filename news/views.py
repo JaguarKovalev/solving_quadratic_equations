c646406bfdc6f06c8c727c76f6b8220ba9db880c
JaguarKovalev <jaguarkovalev@gmail.com>
2024-12-07 15:39:40 +0300

    Add news

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News
+
 
 def news_list(request):
     news_items = News.objects.all()
@@ -8,3 +9,7 @@ def news_list(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'news/news_list.html', {'page_obj': page_obj})
+
+def news_detail(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    return render(request, 'news/news_detail.html', {'news': news})
